200deac6bf0a23b981153c7190fba00e402d6905
joshflo308-crypto <joshflo308@gmail.com>
2026-08-26 19:30:57 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import VoiceCraft from './VoiceCraft.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VoiceCraft />
+  </React.StrictMode>
+)
 
