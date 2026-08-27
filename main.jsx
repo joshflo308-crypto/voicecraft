76118a664e607f0e421c28bcb0956338dab505ab
joshflo308-crypto <joshflo308@gmail.com>
2026-08-26 19:17:08 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import VoiceCraft from './VoiceCraft.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VoiceCraft />
+  </React.StrictMode>
+)
