1bef90d6b9fa58a3c40a70094f9886ef5f362fc0
joshflo308-crypto <joshflo308@gmail.com>
2026-08-26 19:21:28 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import VoiceCraft from './VoiceCraft.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <VoiceCraft />
-  </React.StrictMode>
-)
